1967c4a6775ef80a70d65dd610662b4eb83d00a7
nguyenhuudo02 <nguyenhoanganhdo123@gmail.com>
2023-11-02 13:39:38 +0700

    fist package

---
+++ README.md
@@ -0,0 +1,4 @@
+# Structure HTML Package
+**1. `composer init`**
+
+**2. `composer require package @dev `**
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'nguyenhuudo/nguyenhuudo',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'nguyenhuudo/nguyenhuudo' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'nguyenhuudo/packages' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'e1649248a662cdcbf939621f917670702bb6494f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../nguyenhuudo/packages',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
